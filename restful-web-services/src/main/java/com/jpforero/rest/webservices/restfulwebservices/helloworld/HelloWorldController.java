a2430d6ab293b5e4bf937949d49854da5426b9d8
Jean Paul Forero <jeanpaulforero@gmail.com>
2022-11-24 09:20:35 -0500

    Improve to @GetMapping

--- restful-web-services/src/main/java/com/jpforero/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/jpforero/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,11 @@
 package com.jpforero.rest.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
     }
