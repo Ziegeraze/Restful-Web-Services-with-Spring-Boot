daa42fd6fcae1d9d7f9c2965c6ea20a20752e451
Jean Paul Forero <jeanpaulforero@gmail.com>
2022-11-25 13:04:38 -0500

    Implementing Validations

--- restful-web-services/src/main/java/com/jpforero/rest/webservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/jpforero/rest/webservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.jpforero.rest.webservices.restfulwebservices.exception;
 
 import com.jpforero.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -28,6 +32,14 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
                 ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                "Total Errors: " + ex.getErrorCount() + " First Error: " + Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(), request.getDescription(false));
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
 }
--- restful-web-services/src/main/java/com/jpforero/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/jpforero/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.jpforero.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +31,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.saveOne(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
