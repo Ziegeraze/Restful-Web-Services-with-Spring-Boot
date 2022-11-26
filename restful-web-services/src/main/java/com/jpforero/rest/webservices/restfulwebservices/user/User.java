823cc7c6ef77b9186bee3f2102b41f5eb8c5f859
Jean Paul Forero <jeanpaulforero@gmail.com>
2022-11-26 06:48:44 -0500

    Customize field name in the response

--- restful-web-services/src/main/java/com/jpforero/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/jpforero/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.jpforero.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +10,7 @@ public class User {
 
     private Integer id;
 
+    @JsonProperty("user_name")
     @Size(min=2, message =  "Name should have at least 2 characters")
     private String name;
 
